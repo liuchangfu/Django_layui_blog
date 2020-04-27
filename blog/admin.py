863eb58079a1b6fb9e9f8ac8706a31b49ba8cdfe
空空如也 <shift_1220@163.com>
2020-04-27 17:23:21 +0800

    提交文件

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,54 @@
 from django.contrib import admin
+from blog.models import Post, Category, Tag, AboutMe, Banner, ChickenSoup
+
 
 # Register your models here.
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_time', 'modified_time', 'category', 'author']
+    fields = ['title', 'body', 'avatar', 'excerpt', 'category', 'tags']
+    list_display_links = ['title']
+    search_fields = ['title']
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['name']
+    search_fields = ['name']
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['name']
+    search_fields = ['name']
+
+
+class AboutMeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description']
+    list_display_links = ['description']
+
+
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description']
+    list_display_links = ['description']
+
+
+class ChickenSoupAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description']
+    list_display_links = ['description']
+
+
+# 注册数据表后，后台可以添加
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(AboutMe, AboutMeAdmin)
+admin.site.register(Banner, BannerAdmin)
+admin.site.register(ChickenSoup, ChickenSoupAdmin)
 
 # 修改头部信息和标题信
 admin.site.site_header = '博客后台管理'
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,97 @@
+import markdown
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
 # Create your models here.
+from django.utils.html import strip_tags
+
+
+# 文章分类
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='文章分类')
+
+    class Meta:
+        verbose_name = '文章分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+# 文章标签
+class Tag(models.Model):
+    name = models.CharField(max_length=100, verbose_name='标签名称')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '标签分类'
+        verbose_name_plural = verbose_name
+
+
+# 文章
+class Post(models.Model):
+    title = models.CharField(max_length=70, verbose_name='标题')
+    body = models.TextField(verbose_name='正文')
+    avatar = models.ImageField(upload_to='article/%Y%m%d/', blank=True)
+    created_time = models.DateTimeField(default=timezone.now, verbose_name='创建时间')
+    modified_time = models.DateTimeField(verbose_name='修改时间')
+    excerpt = models.CharField(max_length=100, blank=True, verbose_name='文章摘要')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='文章分类')
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name='文章分类')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
+    views = models.PositiveIntegerField(default=0, editable=False)
+
+    class Meta:
+        verbose_name = '发布管理'
+        verbose_name_plural = verbose_name
+        ordering = ['-created_time']
+
+    def __str__(self):
+        return self.title
+
+    # 截取文章正文前面54个字符为文章摘要
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+        ])
+        self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
+
+# Banner图
+class Banner(models.Model):
+    title = models.CharField(max_length=70, verbose_name='标题')
+    description = models.TextField(verbose_name='Banner图文本信息')
+    banner = models.ImageField(upload_to='banner/%Y%m%d/', blank=True)
+    created_time = models.DateTimeField(default=timezone.now, verbose_name='创建时间')
+
+    class Meta:
+        verbose_name = 'Banner图管理'
+        verbose_name_plural = verbose_name
+
+
+# 心灵鸡汤
+class ChickenSoup(models.Model):
+    description = models.TextField(verbose_name='心灵鸡汤文字描述')
+
+    class Meta:
+        verbose_name = '鸡汤'
+        verbose_name_plural = verbose_name
+
+
+# 关于我
+class AboutMe(models.Model):
+    description = models.TextField(verbose_name='我的信息')
+
+    class Meta:
+        verbose_name = '关于我'
+        verbose_name_plural = verbose_name
